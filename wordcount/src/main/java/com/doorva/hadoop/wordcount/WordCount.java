62ec6d0dfea2804bac4aa14ddce9748a9226e506
doorva <doorva.sharma@gmail.com>
2016-04-03 09:36:01 -0400

    Committing changes related to the wordcount example for Hadoop Mapreduce

---
+++ wordcount/src/main/java/com/doorva/hadoop/wordcount/WordCount.java
@@ -0,0 +1,44 @@
+package com.doorva.hadoop.wordcount;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import com.doorva.hadoop.wordcount.mapreduce.WordMapper;
+import com.doorva.hadoop.wordcount.mapreduce.WordReducer;
+
+public class WordCount extends Configured implements Tool {
+
+	public static void main(String[] args) throws Exception {
+		int exitCode = ToolRunner.run(new WordCount(), args);
+		System.exit(exitCode);
+	}
+
+	@Override
+	public int run(String[] args) throws Exception {
+		if (args.length < 2) {
+			System.out.println("Please give Input & Output Directories Properly");
+			return -1;
+		}
+		JobConf conf = new JobConf(WordCount.class);
+		conf.setJobName("wordCount");
+		FileInputFormat.setInputPaths(conf, new Path(args[0]));
+		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+		conf.setMapperClass(WordMapper.class);
+		conf.setReducerClass(WordReducer.class);
+		conf.setMapOutputKeyClass(Text.class);
+		conf.setMapOutputValueClass(IntWritable.class);
+		conf.setOutputKeyClass(Text.class);
+		conf.setOutputValueClass(IntWritable.class);
+		JobClient.runJob(conf);
+		return 0;
+	}
+
+}
